e814ac0c369f4e5bf1444924c287c0c1080b0241
Kalicz Péter <kaliczp@gmail.com>
2024-02-17 17:34:17 +0100

    Import improved
    Now excel sheet name given by currsheet object. Station name substarcted from the excel sheet. Full dataset is extracted and indexing updated to the extended data.frame.

--- WalterLieth.R
+++ WalterLieth.R
@@ -1,6 +1,7 @@
 ## Import
 library(readxl)
-rawstation <- read_excel("csapadék és hőmérséklet.xlsx", range = "M01!D10:O48", col_names = FALSE, col_types = "numeric")
+currsheet <- "M01"
+stationname <- as.character(read_excel("csapadék és hőmérséklet.xlsx", range = paste0(currsheet,"!C1"), col_names = FALSE))
+rawstation <- read_excel("csapadék és hőmérséklet.xlsx", range = paste0(currsheet,"!D7:O48"), col_names = FALSE, col_types = "numeric")
 library(climatol)
-diagwl(as.data.frame(rawstation[c(1,39,38),]),
-       cols=NULL,est="M01",alt=100,per="1999-2022",mlab="en")
+diagwl(as.data.frame(rawstation[c(4,42,41),]),cols=NULL,stname=stationname,per="1999-2022",mlab="en")
