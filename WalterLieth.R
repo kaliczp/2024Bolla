87edfe11f81834b98efc60aed74e9a8b46c3ad55
Kalicz Péter <kaliczp@gmail.com>
2024-02-17 15:24:03 +0100

    Import first series and plot

--- WalterLieth.R
+++ WalterLieth.R
@@ -1,3 +1,6 @@
+## Import
+library(readxl)
+rawstation <- read_excel("csapadék és hőmérséklet.xlsx", range = "M01!D10:O48", col_names = FALSE, col_types = "numeric")
 library(climatol)
-data(climatol_data)
-diagwl(datcli,cols=NULL,est="My Airport",alt=100,per="1961-90",mlab="en")
+diagwl(as.data.frame(rawstation[c(1,39,38),]),
+       cols=NULL,est="M01",alt=100,per="1999-2022",mlab="en")
