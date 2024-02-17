4ea2fdf1f8df7c3cfe902ade2fb853985c2a4ddd
Kalicz Péter <kaliczp@gmail.com>
2024-02-17 21:53:58 +0100

    Save all sheets into pdf

--- WalterLieth.R
+++ WalterLieth.R
@@ -1,7 +1,11 @@
 ## Import
 library(readxl)
-currsheet <- "M01"
+library(climatol)
+excelsheets <- c("M01", "M03", "M09", "M15", "M16", "M17", "M19", "M21")
+pdf()
+for(currsheet in excelsheets) {
 stationname <- as.character(read_excel("csapadék és hőmérséklet.xlsx", range = paste0(currsheet,"!C1"), col_names = FALSE))
 rawstation <- read_excel("csapadék és hőmérséklet.xlsx", range = paste0(currsheet,"!D7:O48"), col_names = FALSE, col_types = "numeric")
-library(climatol)
 diagwl(as.data.frame(rawstation[c(4,42,41),]),cols=NULL,stname=stationname,per="1999-2022",mlab="en")
+}
+dev.off()
