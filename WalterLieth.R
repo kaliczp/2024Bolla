b552b24eccdd51ba1c80e62957adb84c245bb9e1
Kalicz Péter <kaliczp@gmail.com>
2024-02-19 16:14:06 +0100

    Read monthly values for one station

--- WalterLieth.R
+++ WalterLieth.R
@@ -1,4 +1,4 @@
-## Import
+## Import 1999–2022
 library(readxl)
 library(climatol)
 excelsheets <- c("M01", "M03", "M09", "M15", "M16", "M17", "M19", "M21")
@@ -9,3 +9,11 @@ rawstation <- read_excel("csapadék és hőmérséklet.xlsx", range = paste0(cur
 diagwl(as.data.frame(rawstation[c(4,42,41),]),cols=NULL,stname=stationname,per="1999-2022",mlab="en")
 }
 dev.off()
+
+## Import 2017-21 és 2022
+excelcols <- c("U", "X", "AA", "AD", "AG")
+currcol <- excelcols[1]
+## Read raw cols
+rawtemp <- read_excel("csapadék és hőmérséklet.xlsx", range = paste0("Havi T!",currcol,"2:",currcol,"73"), col_names = FALSE, col_types = "numeric")
+## 
+matrix(as.numeric(rawtemp[,1,drop = TRUE]), ncol = 12, byrow = TRUE)
